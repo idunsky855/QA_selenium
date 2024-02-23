85765ed74c0d925042ee46dfdaceec8e5834af6b
idunsky855 <idunsky855@gmail.com>
2024-02-23 08:42:20 +0200

    modified redundent imports

--- src/test/java/pom_test_cases/Payment.java
+++ src/test/java/pom_test_cases/Payment.java
@@ -6,31 +6,11 @@ import org.junit.internal.TextListener;
 import org.junit.runner.JUnitCore;
 import org.junit.Before;
 import org.junit.After;
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.core.IsNot.not;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import dev.failsafe.internal.util.Assert;
-
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.Keys;
 import java.util.*;
-import java.net.MalformedURLException;
-import java.net.URL;
-import org.openqa.selenium.support.ui.Select;
-
 import org.apache.logging.log4j.*;
 
 public class Payment {
